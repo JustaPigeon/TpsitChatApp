7e9040de500609d3539c623a91266ac03204517a
JustaPigeon <147087233+JustaPigeon@users.noreply.github.com>
2024-03-06 11:49:03 +0100

    Add files via upload

---
+++ ThreadChatClient.java
@@ -0,0 +1,56 @@
+import java.awt.*; 
+import java.net.*; 
+import java.io.*; 
+import javax.swing.*;
+
+public class ThreadChatClient implements Runnable {
+    private List lista; 
+    Thread me; 
+    
+    Private Socket client; 
+    Private BufferedReader input = null; 
+    PrintWriter input = null; 
+
+    public ThreadChatClient(List lis, String ipServer, int porta)
+    {
+        this.lista = lis; 
+        try{
+            client = new Socket(ipServer, porta);
+            this.input = new BufferedReader(new InputStreamReader(client.getInputStream())); 
+            this.output = new PrintWriter(client.getOutputStream(), true);
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Impossibile Connettersi!");
+        }
+        me = new Thread(this); 
+        me.start();
+    }
+
+    public void run()
+    {
+        while (true) {
+            try{
+                String mex = null; 
+                while((mex=input.readLine())==null)
+                {
+                    
+                }
+                lista.add(mex);
+                lista.select(lista.getItemCount()-1);
+
+            }catch(Exception e)
+            {
+
+            }
+        }
+    }
+
+    public void spedisciMessaggioChat(String messaggio)
+    {
+        try{
+            output.println(messaggio); 
+        }catch(Exception e)
+        {
+            
+        }
+    }
+}
---
+++ ThreadChatConnessioni.java
@@ -0,0 +1,55 @@
+import java.net.*; 
+import java.io.*; 
+
+public class ThreadChatConnessioni implements Runnable{
+    private ThreadGesnetionServizioChat gestoreChat; 
+
+    private Socket client=null;
+    private BufferedReader input = null;
+    private PrintWriter output = null; 
+    Thread me; 
+
+    public ThreadChatConnessioni(ThreadGestionServizioChat gs, Socket cl)
+    {
+        this.gestoreChat = gs; 
+        this.client = cl; 
+        try{
+            this.input = new BufferedReader(new InputStreamReader(client.getInputStream())); 
+            this.output = new PrintWriter(this.client.getOutputStream(), true);
+        }catch(Exception e)
+        {
+            output.println("Errore!");
+        }
+        me=new Thread(this); 
+        me.start(); 
+    }
+
+    public void run()
+    {
+        while (true) {
+            try{
+                String mex = null; 
+                while((mex=input.readLine())==null)
+                {
+
+                }
+                
+                gestoreChat.spedisciMessaggio(mex);
+            }catch(Exception e)
+            {
+                output.println("Errore nella spedizione!");
+            }
+        }
+    }
+
+
+    public void spedisciMessaggioChat(String messaggio)
+    {
+        try{
+            output.println(messaggio);
+        }catch(Exception e)
+        {
+            output.println("Errore spedizione singolo messaggio");
+        }
+    }
+}
